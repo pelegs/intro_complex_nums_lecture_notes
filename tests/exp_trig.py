5b569d47036ab9d7f510aed621cae0f9d8b8004a
pelegs <pelegs@gmail.com>
2025-10-17 23:05:49 +0200

    'Dashes' work (will change to actual dashes later)

--- tests/exp_trig.py
+++ tests/exp_trig.py
@@ -10,12 +10,13 @@ x = np.linspace(0, 2 * np.pi, num_ticks)
 im_xs = np.ones(num_ticks)
 real_zs = -1 * np.ones(num_ticks)
 
-# ax.plot(np.cos(x), np.sin(x), zs=0, zdir="y", label="Circle")
 (exp_plot,) = ax.plot(
     xs=[0, 1], ys=[0, 0], zs=0, zdir="y", label="exp(it)", linewidth=2
 )
 (im_plot,) = ax.plot(t, np.sin(t), zs=1, zdir="x", label="sin(t)", linewidth=2)
+(im_dashes,) = ax.plot([1, 1], [0, 0], zs=0, zdir="y", c="gray")
 (real_plot,) = ax.plot(np.cos(t), t, zs=-1, label="cos(t)", linewidth=2)
+(real_dashes,) = ax.plot([1, 1], [0, 0], zs=0, zdir="y", c="gray")
 
 
 ax.set_aspect("equal", "box")
@@ -38,9 +39,14 @@ ax.view_init(elev=20.0, azim=40, roll=0)
 def update_animation(frame):
     ph = 2 * np.pi / num_frames * frame
     exp_plot.set_data_3d([0, np.cos(ph)], [0, 0], [0, np.sin(ph)])
+
     im_plot.set_data_3d(im_xs, t, np.sin(t + ph))
     real_plot.set_data_3d(np.cos(t + ph), t, real_zs)
-    return [exp_plot, im_plot, real_plot]
+
+    im_dashes.set_data_3d([np.cos(ph), 1], [0, 0], [np.sin(ph), np.sin(ph)])
+    real_dashes.set_data_3d([np.cos(ph), np.cos(ph)], [0, 0], [np.sin(ph), -1])
+
+    return [exp_plot, im_plot, real_plot, im_dashes, real_dashes]
 
 
 num_frames = 36 * 4
