74708906b5f5573f247f451ef5904bba199528bf
pelegs <pelegs@gmail.com>
2025-10-23 17:54:12 +0200

    Kinda works now

--- scripts/s_plane.py
+++ scripts/s_plane.py
@@ -1,28 +1,45 @@
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import cm
 from matplotlib.backend_bases import MouseButton
 
-
-def on_click(event):
-    if event.button is MouseButton.LEFT:
-        print(event.xdata, event.ydata)
-
+num_digits = 2
 
 num_steps = 250
 ts = np.linspace(0, 2 * np.pi, num_steps)
-z = -0.05 + 1.0j
+z = -0.2 + 1.0j
 # temp
 ws = np.exp(z * ts)
+
+
+def plot_z(z):
+    global cmplx_func
+    ws = np.exp(z * ts)
+    cmplx_func.remove()
+    (cmplx_func,) = ax_func.plot(np.real(ws), np.imag(ws), linewidth=2, color="blue")
+    plt.gcf().canvas.draw_idle()
+
+
+def on_click(event):
+    global z
+    if event.button is MouseButton.LEFT:
+        a = round(event.xdata, num_digits)
+        b = round(event.ydata, num_digits)
+        z = a + b * 1.0j
+        plot_z(z)
+        # print(f"z_old = {z}")
+        # print(f"z_new = {new_z}")
+
 
 # -------- #
 #  Figures #
 # -------- #
 
-gs = gridspec.GridSpec(1, 2, height_ratios=[1] * 1, width_ratios=[1] * 2)
+fig, axes = plt.subplots(ncols=2, nrows=1, constrained_layout=True)
 
-ax_s_plane = plt.subplot(gs[0, 0])
-ax_func = plt.subplot(gs[0, 1])
+ax_s_plane = axes[0]
+ax_func = axes[1]
 
 ax_s_plane.grid()
 ax_s_plane.set_aspect("equal", "box")
@@ -35,9 +52,10 @@ ax_func.grid()
 ax_func.set_aspect("equal", "box")
 ax_func.set_xlabel("Real")
 ax_func.set_ylabel("Imaginary")
-ax_func.set_xlim(-2, 2)
-ax_func.set_ylim(-2, 2)
-ax_func.plot(np.real(ws), np.imag(ws), linewidth=2)
+ax_func.set_xlim(-5, 5)
+ax_func.set_ylim(-5, 5)
+(cmplx_func,) = ax_func.plot(np.real(ws), np.imag(ws), linewidth=2)
+plot_z(z)
 
 # ------------------ #
 #  Connecting stuff  #
