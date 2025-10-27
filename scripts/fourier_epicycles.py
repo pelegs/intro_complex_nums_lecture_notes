43327c19b63078ca305296e19196973d3c389351
pelegs <pelegs@gmail.com>
2025-10-27 12:37:35 +0100

    Started work on proper epicycles script

--- scripts/fourier_epicycles.py
+++ scripts/fourier_epicycles.py
@@ -1,14 +1,46 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-if __name__ == "__main__":
-    # test stuff
-    points = np.array(
-        [
-            1 + 0j,
-            0 + 1j,
-            -1 + 0j,
-            0 - 1j,
-        ]
-    )
-    cs = np.fft.ifft(points, n=40)
-    print(cs)
+
+def as_polar(z):
+    return np.array([np.abs(z), np.angle(z)])
+
+
+def as_cartesian(radii, angles):
+    return radii * np.exp(angles * 1j)
+
+
+pts_spatial = np.array(
+    [
+        1 + 1j,
+        -1 + 1j,
+        -1 + -1j,
+        1 - 1j,
+    ]
+)
+num_steps = 100
+time = np.linspace(0, 2 * np.pi, num_steps)
+
+num_circles = 10
+pts_circles = np.fft.fft(pts_spatial, n=num_circles)
+
+circle_params = as_polar(pts_circles)
+circles = np.zeros(shape=(num_steps, num_circles), dtype=np.complex128)
+for step, t in enumerate(time):
+    circles[step] = as_cartesian(circle_params[0], circle_params[1] * t)
+circles_cumsums = np.cumsum(circles, axis=1)
+
+# Graphics
+fig, ax = plt.subplots()
+ax.set_title("Epicycles?")
+ax.set_xlabel("Real")
+ax.set_ylabel("Imaginary")
+# ax.set_xlim(-2, 2)
+# ax.set_ylim(-2, 2)
+ax.set_aspect("equal", "box")
+ax.grid()
+
+frame_data = circles_cumsums[-1]
+plt.plot(np.real(frame_data), np.imag(frame_data))
+
+plt.show()
