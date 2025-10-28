819f0d3ce8cde9b4e7137d2ca6d2fdc9aab8ba39
pelegs <pelegs@gmail.com>
2025-10-28 11:15:04 +0100

    Added polar form product (still WIP though)

--- scripts/fourier_epicycles.py
+++ scripts/fourier_epicycles.py
@@ -21,13 +21,16 @@ pts_spatial = np.array(
 num_steps = 100
 time = np.linspace(0, 2 * np.pi, num_steps)
 
-num_circles = 10
-pts_circles = np.fft.fft(pts_spatial, n=num_circles)
-
-circle_params = as_polar(pts_circles)
+N = 100
+freqs = np.arange(-N, N + 1, 1).astype(int)
+num_circles = freqs.shape[0]
+pts_circles = np.fft.ifft(pts_spatial, n=num_circles)
+num_circles = pts_circles.shape[0]
+circles_array = np.zeros(shape=(num_steps, num_circles), dtype=np.complex128)
 circles = np.zeros(shape=(num_steps, num_circles), dtype=np.complex128)
 for step, t in enumerate(time):
-    circles[step] = as_cartesian(circle_params[0], circle_params[1] * t)
+    circles_array[step] = np.exp(freqs * 1j * t)
+    circles[step] = pts_circles * circles_array[step]
 circles_cumsums = np.cumsum(circles, axis=1)
 
 # Graphics
