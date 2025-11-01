54b21c3861339fba4de13f4ea51de6e4f1f34319
pelegs <pelegs@gmail.com>
2025-11-01 17:45:59 +0100

    IT WORKS

--- python_scripts/fourier_epicycles.py
+++ python_scripts/fourier_epicycles.py
@@ -1,9 +1,8 @@
-import enum
-
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Circle
+from scipy.integrate import quad_vec
 
 
 def as_polar(z):
@@ -14,7 +13,7 @@ def as_cartesian(radii, angles):
     return radii * np.exp(angles * 1j)
 
 
-pts_spatial = np.array(
+pts_spatial_complex = np.array(
     [
         2 + 2j,
         2 + 1.5j,
@@ -48,19 +47,37 @@ pts_spatial = np.array(
         0.5 + 2j,
         1 + 2j,
         1.5 + 2j,
+        2 + 2j,
     ]
 )
+# pts_spatial_float = np.array([np.real(pts_spatial_complex), np.imag(pts_spatial_complex)])
 
-num_steps = 500
+time_list = np.linspace(0, 2 * np.pi, pts_spatial_complex.shape[0])
+
+num_steps = 1000
 time_series = np.linspace(0, 2 * np.pi, num_steps)
-N = 5
-num_spatial_pts = pts_spatial.shape[0]
-basis_freqs = np.arange(0, 2 * N + 1, 1) * 1j
-# coeffs = np.flip(np.sort(np.random.uniform(1, 23, size=2 * N + 1)))
-coeffs = np.fft.ifft(pts_spatial, 2 * N + 1)
+pts_interpolated = np.interp(time_series, time_list, pts_spatial_complex)
+
+N = 10
+freqs = np.arange(-N, N + 1, 1)
+coeffs = np.zeros(2 * N + 1, dtype=np.complex128)
+for i, k in enumerate(freqs):
+    coeffs[i] = (
+        1
+        / (2 * np.pi)
+        * quad_vec(
+            lambda t: np.interp(t, time_list, pts_spatial_complex)
+            * np.exp(-k * t * 1j),
+            0,
+            2 * np.pi,
+            limit=100,
+            full_output=1,
+        )[0]
+    )
+
 circles = np.zeros(shape=(num_steps, 2 * N + 1), dtype=np.complex128)
 for step, time in enumerate(time_series):
-    circles[step] = coeffs * np.exp(basis_freqs * time)
+    circles[step] = coeffs * np.exp(freqs * 1j * time)
 circles_centers = np.cumsum(circles, axis=1)
 rad_max_sum = np.sum(np.abs(coeffs))
 
@@ -69,12 +86,16 @@ fig, ax = plt.subplots()
 ax.set_title("Epicycles?")
 ax.set_xlabel("Real")
 ax.set_ylabel("Imaginary")
-ax.set_xlim(-2 * rad_max_sum, 2 * rad_max_sum)
-ax.set_ylim(-2 * rad_max_sum, 2 * rad_max_sum)
+ax.set_xlim(-4, 4)
+ax.set_ylim(-4, 4)
 ax.set_aspect("equal", "box")
 ax.grid()
 
-(plot_lines,) = plt.plot(np.real(circles_centers[0]), np.imag(circles_centers[0]))
+ax.plot(np.real(pts_interpolated), np.imag(pts_interpolated), "o")
+(plot_lines,) = ax.plot(np.real(circles_centers[0]), np.imag(circles_centers[0]))
+(total_path,) = ax.plot(
+    [np.real(circles_centers[0, -1])], [np.imag(circles_centers[0, -1])], c="red"
+)
 circle_patches = []
 for circle, coeff in zip(circles_centers[0], coeffs):
     circle_patch = Circle(
@@ -88,7 +109,11 @@ def update_animation(step):
     plot_lines.set_data(np.real(circles_centers[step]), np.imag(circles_centers[step]))
     for circle, circle_patch in zip(circles_centers[step], circle_patches):
         circle_patch.center = [np.real(circle), np.imag(circle)]
-    return [plot_lines, circle_patches]
+    total_path.set_data(
+        [np.real(circles_centers[:step, -1])], [np.imag(circles_centers[:step, -1])]
+    )
+    return [plot_lines, circle_patches, total_path]
+    # return [total_path]
 
 
 animation = FuncAnimation(fig=fig, func=update_animation, frames=num_steps, interval=0)
